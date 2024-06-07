5c5b729272b3dca9548cf9a7d4a0a358e0369e50
shilpifd <sk.eduworks@gmail.com>
2024-06-07 14:08:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ background-size: 100% 100%">
 <?php
 $firstname=$_POST['firstname'];
 $email=$_POST['email'];
-$servername = "sudipproject-rds.cinsyk8qt8ou.us-west-2.rds.amazonaws.com";
+$servername = "shilpiproject-rds-inst.ch0y6u6cuw6k.us-east-1.rds.amazonaws.com";
 $username = "admin";
 $password = "intel123";
 $db = "intel";
